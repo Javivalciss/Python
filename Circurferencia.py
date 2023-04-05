bcb6d5c4ffa88d4e04fe165df8d122111390ab8f
Javivalciss <128632505+Javivalciss@users.noreply.github.com>
2023-04-05 14:47:24 -0400

    Add files via upload

---
+++ Circurferencia.py
@@ -0,0 +1,4 @@
+pi= 3.141592
+radio = float( input("Radio con el que desea calcular la Circunferencia :"))
+Circunferencia = float(2*pi*radio)
+print("La circunferencia del radio ", radio ,"es de : " ,Circunferencia )
